09699ac518218092d9eefe977322f530c5978413
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2023-12-30 22:51:18 -0600

    Update index-Yg90K5_M.js

--- assets/index-Yg90K5_M.js
+++ assets/index-Yg90K5_M.js
@@ -2684,9 +2684,3 @@ function __vite__mapDeps(indexes) {
   }
   return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
 }
-
-document.body.addEventListener("keydown", function (event) {
-        if (event.key === "Escape") {
-            window.location.replace("https://google.com/");
-        }
-    });
